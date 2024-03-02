86471dabafad896e69596cfb2422f932016fcaf8
AbdullahKadad <qdadabdullah@gmail.com>
2024-03-02 15:17:50 +0000

    initial backend files

---
+++ schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Branch (
+    branch_id SERIAL PRIMARY KEY,
+    branch_name VARCHAR(100),
+    location VARCHAR(255)
+);
+
+CREATE TABLE Room (
+    room_id SERIAL PRIMARY KEY,
+    branch_id INT NOT NULL,
+    room_number VARCHAR(20) NOT NULL,
+    capacity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    is_available BOOLEAN NOT NULL DEFAULT TRUE,
+    FOREIGN KEY (branch_id) REFERENCES Branch(branch_id) ON DELETE CASCADE
+);
+
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    phone INT NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL
+    
+);
+
+CREATE TABLE Reservation (
+    reservation_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    room_id INT NOT NULL,
+    check_in_date DATE NOT NULL,
+    check_out_date DATE NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (room_id) REFERENCES Room(room_id) ON DELETE CASCADE
+);
---
+++ server.js
@@ -0,0 +1,46 @@
+"use strict";
+// Import required modules
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+// Create an Express application
+const app = express();
+
+// Server PORT
+const PORT = process.env.PORT;
+
+// Enable CORS middleware
+app.use(cors());
+
+// Enable JSON parsing middleware
+app.use(express.json());
+
+// Database config
+const { Client } = require("pg");
+const url = process.env.DATABASE_URL;
+const client = new Client(url);
+
+// Define your other routes here...
+
+// Route handler for the root route "/"
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the home page!");
+});
+
+// Middleware to handle page not found (404) errors
+app.use((req, res, next) => {
+  res.status(404).send("Page not found");
+});
+
+// Middleware to handle internal server errors (500)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
