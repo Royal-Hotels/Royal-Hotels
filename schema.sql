02f3a1e9a5187529d3a78223758b4e0d420c562b
AbdullahKadad <qdadabdullah@gmail.com>
2024-03-03 18:23:34 +0000

    end day -clean up

--- schema.sql
+++ schema.sql
@@ -1,8 +1,10 @@
+
 CREATE TABLE branch (
     branch_id SERIAL PRIMARY KEY,
     branch_name VARCHAR(100),
     location VARCHAR(255)
 );
+
 
 CREATE TABLE room (
     room_id SERIAL PRIMARY KEY,
@@ -11,17 +13,16 @@ CREATE TABLE room (
     capacity INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     is_available BOOLEAN NOT NULL DEFAULT TRUE,
-    FOREIGN KEY (branch_id) REFERENCES Branch(branch_id) ON DELETE CASCADE
+    FOREIGN KEY (branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
 );
 
-CREATE TABLE user (
+CREATE TABLE "users" (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL,
     phone VARCHAR(10) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     is_admin BOOLEAN NOT NULL DEFAULT FALSE
-    
 );
 
 CREATE TABLE reservation (
@@ -31,6 +32,6 @@ CREATE TABLE reservation (
     check_in_date DATE NOT NULL,
     check_out_date DATE NOT NULL,
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (room_id) REFERENCES Room(room_id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (room_id) REFERENCES room(room_id) ON DELETE CASCADE
 );
